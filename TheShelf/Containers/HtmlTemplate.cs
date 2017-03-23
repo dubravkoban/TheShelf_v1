dcc97e5f4ab784111543d3c20d9bf18e6b37e53c
dubravkoban <dubravko.ban@gmail.com>
2017-03-23 22:25:28 +0100

    first commit

---
+++ TheShelf/Containers/HtmlTemplate.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheShelf.Containers
+{
+    public class HtmlTemplate
+    {
+        private string _filePath = ConfigurationManager.AppSettings["FilePath"];
+        private int _byteCount = 0;
+
+        public HtmlTemplate()
+        {
+            MemoryStream mStream = null;
+            FileStream fStream = null;
+
+            try
+            {
+                ITag tag = new HtmlTag();
+                InsertTags(fStream, mStream, tag);
+
+                tag = new HeadTag();
+                InsertTags(fStream, mStream, tag);
+
+                tag = new BodyTag();
+                InsertTags(fStream, mStream, tag);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                mStream.Dispose();
+                fStream.Dispose();
+            }
+        }
+
+        private FileStream InsertTags(FileStream fStream, MemoryStream mStream, ITag tag)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes($"{tag.OpeningTag} ¤ {tag.ClosingTag}");
+            mStream = new MemoryStream(bytes);
+
+            if (!File.Exists(_filePath))
+            {
+                fStream = File.Create(_filePath);
+
+                mStream.Seek(0, SeekOrigin.Begin);
+                mStream.WriteTo(fStream);
+
+                _byteCount = bytes.Count();
+            }
+            else
+            {
+                // need to find a way to replace the crmklja value (63) inside fStream with value of bytes variable
+
+                byte[] buffer = null;
+                fStream.Read(buffer, 0, _byteCount);
+            }
+            
+            return fStream;
+
+            //if (File.Exists(_filePath))
+            //{
+            //    IEnumerable<string> lines = File.ReadLines(_filePath).Select(l => l.Replace("¤", tag.OpeningTag + "¤" + tag.ClosingTag));
+            //    File.WriteAllLines(_filePath, lines);
+            //}
+            //else
+            //{
+            //    IEnumerable<string> lines = new List<string>
+            //    {
+            //        tag.OpeningTag,
+            //        "¤",
+            //        tag.ClosingTag
+            //    };
+
+            //    File.WriteAllLines(_filePath, lines);
+            //}
+        }
+    }
+
+    internal class HtmlTag : ITag
+    {
+        public string OpeningTag { get; set; }
+        public string ClosingTag { get; set; }
+
+        public HtmlTag()
+        {
+            OpeningTag = "<html>";
+            ClosingTag = "</html>";
+        }
+    }
+
+    internal class HeadTag : ITag
+    {
+        public string OpeningTag { get; set; }
+        public string ClosingTag { get; set; }
+
+        public HeadTag()
+        {
+            OpeningTag = "<head>";
+            ClosingTag = "</head>";
+        }
+    }
+
+    internal class BodyTag : ITag
+    {
+        public string OpeningTag { get; set; }
+        public string ClosingTag { get; set; }
+
+        public BodyTag()
+        {
+            OpeningTag = "<body>";
+            ClosingTag = "</body>";
+        }
+    }
+
+    interface ITag
+    {
+        string OpeningTag { get; set; }
+        string ClosingTag { get; set; }
+    }
+}
---
+++ TheShelf/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShelf.Containers;
+
+namespace TheShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HtmlTemplate template = new HtmlTemplate();
+
+            char[] cmrklja = { '¤' };
+            byte[] bytes = Encoding.ASCII.GetBytes(cmrklja);
+
+            Console.WriteLine(Convert.ToInt32(bytes[0]));
+            Console.ReadKey();
+
+            //var lines = new List<string>
+            //{
+            //    "first",
+            //    "čmrklja",
+            //    "third"
+            //};
+
+            //byte[] bytes = Encoding.ASCII.GetBytes(lines[0] + lines[1] + lines[2]);
+            //MemoryStream stream1 = new MemoryStream(bytes);
+            //using (FileStream fileStream = File.Create(@"C:\Temp\Tempy.txt"))
+            //{
+            //    stream1.Seek(0, SeekOrigin.Begin);
+            //    stream1.WriteTo(fileStream);
+            //}
+
+            //using (var file = new FileStream(@"C:\Temp\test.txt", FileMode.OpenOrCreate))
+            //using (StreamWriter stream = new StreamWriter(file))
+            //{
+            //    stream.WriteLine("bljeeeeeeeeee1");
+            //    stream.WriteLine("bljeeeeeeeeee2");
+            //    stream.WriteLine("bljeeeeeeeeee3");
+            //    stream.WriteLine("¤");
+            //}
+
+            //var lines = File.ReadLines(@"C:\Temp\test.txt").Select(l => l.Replace("¤", "This has replaced čmrklja"));
+            //File.WriteAllLines(@"C:\Temp\test.txt", lines);
+        }
+    }
+}
